2e3f2dd1af73aa377903e806f13ae71eba567f46
avinassh <avinashsajjan@gmail.com>
2014-02-16 22:10:58 +0530

    updated utils, now it uses http://gutenberg.myebook.bg mirror

--- utils.py
+++ utils.py
@@ -10,23 +10,25 @@ from bs4 import BeautifulSoup
 def dowload_top_100():
     '''This script will download Top 100 books of last 30 days from Project 
     Gutenberg and saves them with appropriate file name'''
-    base_url = Template('http://www.gutenberg.org/files/$book_id/$book_id.txt')
+    base_url = 'http://www.gutenberg.myebook.bg/'
     r = requests.get('http://www.gutenberg.org/browse/scores/top')
     soup = BeautifulSoup(r.text)
     h2 = soup.find(id='books-last30')
     ol = h2.next_sibling.next_sibling
     for a_tag in ol.find_all('a'):
         m = re.match(r'(.*)(\(\d+\))', a_tag.text)
-        file_name = m.group(1).strip()
+        book_name = m.group(1).strip()
         m = re.match(r'/ebooks/(\d+)', a_tag.get('href'))
         book_id = m.group(1)
-        url = base_url.substitute(book_id=book_id)
+        # ugh, I know this is ugly.
+        url = base_url + '/'.join(list(book_id[:-1])) + '/' + book_id + '/' + book_id + '.txt'
         r = requests.get(url)
         if r.status_code == requests.codes.ok:
-            #print 'Downloaded... ', file_name
-            f = open(file_name, 'w')
-            f.write(r.text.encode('UTF-8'))
-            f.close()
+            # print 'Downloaded... ', file_name
+            with open(file_name, 'w') as f:
+                f.write(r.text.encode('UTF-8'))
+        else:
+            print 'Failed for ', book_id
 
 def get_cleaned(text_corpus):
     '''Removes all the punctuation and special characters from corpus'''
@@ -38,4 +40,4 @@ def get_high_frequency_words():
         yield record['term']
 
 if __name__ == '__main__':
-    dowload_top_100()
+    pass
